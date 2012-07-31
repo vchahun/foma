f4167828a4618a2ff24666b81f9fb6705216c257
Victor Chahuneau <vchahune@cs.cmu.edu>
2012-07-30 23:18:55 -0400

    Unicode input/output

--- python/foma.pyx
+++ python/foma.pyx
@@ -1,23 +1,33 @@
 from libc.stdlib cimport free
 
+cdef char* as_str(word):
+    cdef bytes res
+    if isinstance(word, bytes):
+        res = word
+    elif isinstance(word, unicode):
+        res = word.encode('utf8')
+    else:
+        raise TypeError('Expected string and not {0}'.format(type(word)))
+    return res
+
 cdef class MEDMatch:
     cdef public int cost
-    cdef public bytes instring
-    cdef public bytes outstring
+    cdef public unicode instring
+    cdef public unicode outstring
 
-    def __cinit__(self, int cost, char* instring, char* outstring):
+    def __cinit__(self, int cost, bytes instring, bytes outstring):
         self.cost = cost
-        self.instring = instring
-        self.outstring = outstring
+        self.instring = unicode(instring, 'utf8')
+        self.outstring = unicode(outstring, 'utf8')
 
     def __str__(self):
         return self.outstring
 
-def read_binary(char* filename):
-    return FSM(binary=filename)
+def read_binary(filename):
+    return FSM(binary=as_str(filename))
 
-def read_text(char* filename):
-    return FSM(text=filename)
+def read_text(filename):
+    return FSM(text=as_str(filename))
 
 cdef class FSM:
     cdef fsm* net
@@ -25,50 +35,54 @@ cdef class FSM:
     def __cinit__(self, binary=None, text=None):
         if text:
             self.net = fsm_read_text_file(<char *> text)
+            if not self.net:
+                raise IOError('cannot read text file {0}'.format(text))
         elif binary:
             self.net = fsm_read_binary_file(<char *> binary)
-        else:
-            self.net = fsm_create('')
+            if not self.net:
+                raise IOError('cannot read binary fsm {0}'.format(binary))
 
     def __dealloc__(self):
         if self.net != NULL:
             fsm_destroy(self.net)
 
-    def write(self, char* filename):
+    def write(self, filename):
         if fsm_write_binary_file(self.net, filename):
             raise IOError('cannot write FSM to \'%s\'' % filename)
 
-    def apply_up(self, char* word):
+    def apply_up(self, word):
         cdef apply_handle* applyh = apply_init(self.net)
-        cdef char* result = apply_up(applyh, word)
+        cdef char* result = apply_up(applyh, as_str(word))
         try:
             while True:
                 if result == NULL: break
-                yield result
+                yield unicode(result, 'utf8')
                 result = apply_up(applyh, NULL)
-        finally:
+        finally: 
             apply_clear(applyh)
 
-    def apply_down(self, char* word):
+    def apply_down(self, word):
         cdef apply_handle* applyh = apply_init(self.net)
-        cdef char* result = apply_down(applyh, word)
+        cdef char* result = apply_down(applyh, as_str(word))
         try:
             while True:
                 if result == NULL: break
-                yield result
+                yield unicode(result, 'utf8')
                 result = apply_down(applyh, NULL)
-        finally:
+        finally: 
             apply_clear(applyh)
 
-    def med(self, char* word, int limit=4, int cutoff=15, int heap_max=4194305, char* align=''):
+    def med(self, word, int limit=4, int cutoff=15,
+            int heap_max=4194305, align=None):
         if not self.arity == 1:
             raise NotImplementedError('miminum edit distance is only supported for FSAs')
         cdef apply_med_handle* medh = apply_med_init(self.net)
         apply_med_set_med_limit(medh, limit)
         apply_med_set_med_cutoff(medh, cutoff)
         apply_med_set_heap_max(medh, heap_max)
-        apply_med_set_align_symbol(medh, align)
-        cdef char* result = apply_med(medh, word)
+        if align:
+            apply_med_set_align_symbol(medh, as_str(align))
+        cdef char* result = apply_med(medh, as_str(word))
         cdef int cost
         cdef char *instring, *outstring
         try:
@@ -79,7 +93,7 @@ cdef class FSM:
                 outstring = apply_med_get_outstring(medh)
                 yield MEDMatch(cost, instring, outstring)
                 result = apply_med(medh, NULL)
-        finally:
+        finally: 
             free(medh)
 
     property arity:
